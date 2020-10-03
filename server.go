72330d9cbf17bbedd69d54e10a5b5ce159826a26
Roman <rendonon@gmail.com>
2020-10-03 14:11:10 +0300

    Rename

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "encoding/json"
+  "net/http"
+  "time"
+)
+
+type Time struct {
+  Now string
+}
+
+func main() {
+  http.HandleFunc("/time", mainHandler)
+  http.ListenAndServe(":8795", nil)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+
+  now := time.Now()
+  timeFormated := now.Format(time.RFC3339)
+
+  timeObj := &Time{timeFormated}
+
+  timeJson, _ := json.Marshal(timeObj)
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(timeJson)
+}
